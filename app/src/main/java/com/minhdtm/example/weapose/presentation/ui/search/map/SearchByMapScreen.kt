79ed9b75d93216988f90a60297a30e1bef5e0009
Dương Thái Minh <73929358+hide-your-code@users.noreply.github.com>
2022-07-30 23:26:02 +0700

    [v1.0.3] Update README.md and Upgrade libraries (#5)

--- app/src/main/java/com/minhdtm/example/weapose/presentation/ui/search/map/SearchByMapScreen.kt
+++ app/src/main/java/com/minhdtm/example/weapose/presentation/ui/search/map/SearchByMapScreen.kt
@@ -21,6 +21,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
@@ -32,12 +34,13 @@ import com.minhdtm.example.weapose.presentation.theme.WeaposeTheme
 import com.minhdtm.example.weapose.presentation.ui.WeatherAppState
 import com.minhdtm.example.weapose.presentation.utils.Constants
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun SearchByMap(
     appState: WeatherAppState,
     viewModel: SearchByMapViewModel = viewModel(),
 ) {
-    val state by viewModel.state.collectAsState()
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
     val context = LocalContext.current
 
@@ -215,7 +218,10 @@ fun ResultMap(
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(
+    ExperimentalAnimationApi::class,
+    ExperimentalMaterial3Api::class,
+)
 @Composable
 fun SearchAppBar(
     isDarkMode: Boolean,
